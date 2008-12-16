33c6b7ad5e9caee346ba73a46a613be38b4d666d
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2008-12-16 12:37:54 +0100

    Pretty printing fixups, including extra parens in the output.

--- Europa/Pretty.hs
+++ Europa/Pretty.hs
@@ -5,10 +5,13 @@ import Text.PrettyPrint.Leijen
 
 
 instance Pretty id => Pretty (Expr id a) where
-    pretty (Lam x t _) = parens (pretty x <+> text "=>" <> softline <> pretty t)
+    pretty (Lam x t _) = pretty x <+> text "=>" <+> pretty t
     pretty pi@(Pi x t _) | Pi _ _ _ <- range pi = parens (pretty x) <+> text "->" <+> pretty t
                          | otherwise = pretty x <+> text "->" <+> pretty t
-    pretty (App t1 t2 _) = pretty t1 <+> pretty t2
+    pretty (App t1 t2 _) =
+        let f = if isApplicative t1 then id else parens
+            g = if isAtomic t2 then id else parens
+        in f (pretty t1) <+> g (pretty t2)
     pretty (Var x _) = pretty x
     pretty Type = text "Type"
     pretty Kind = text "Kind"
@@ -17,12 +20,15 @@ instance Pretty id => Pretty (TVar id a) where
     pretty (x ::: ty) = pretty x <+> char ':' <+> pretty ty
     pretty (Hole ty) =  pretty ty
 
-    prettyList xs = vcat $ map (\x -> pretty x <> dot) xs
+    prettyList = vcat . map (\x -> pretty x <> dot)
 
 instance Pretty id => Pretty (Rule id a) where
     pretty (lhs :--> rhs) = pretty lhs <+> text "-->" <+> pretty rhs
 
 instance Pretty id => Pretty (TyRule id a) where
+    pretty ([] :@ rule) = text "[]" <+> pretty rule
     pretty (env :@ rule) =
-        encloseSep (char '[') (char ']') (text ", ") (map pretty env)
+        encloseSep (text "[ ") (text " ]") (text ", ") (map pretty env)
         <+> pretty rule
+
+    prettyList = vcat . map (\x -> pretty x <> dot)
