b22f1e3a684704945b5f57eec6115b85759fbd79
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-01-08 12:25:32 +0100

    Skeletal EuM monad.

---
+++ Europa/EuM.hs
@@ -0,0 +1,19 @@
+module Europa.EuM (EuM, runEuM, warn, warnings
+                  , text
+                  , Exception(..), throw, liftIO) where
+
+import Control.Monad.Trans
+import Control.Exception
+import Text.PrettyPrint.Leijen
+
+
+-- | Register a new warning.
+warn :: String -> EuM ()
+warn = undefined
+
+-- | Get the list of warnings so far.
+warnings :: EuM [Doc]
+warnings = undefined
+
+newtype EuM a = EuM { runEuM :: IO a }
+    deriving (Monad, MonadIO)
