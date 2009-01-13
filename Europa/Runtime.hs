b207d281adc4f4e8fca3b6e5baf6b3ce7c69e636
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-01-13 08:38:34 +0100

    Overhaul of runtime module.
    Datatype for conversion and for typechecking are distinct, as before,
    but constructors for Type, Kind and Pi added in 'Code' for slightly
    better efficiency. Conversion counts down, sort checking counts down and
    type reconstruction counts up.
    
    Conversion is now used in typechecking and works correctly. 'runChecks'
    needs updating. Possible captures in typing of lambda.

--- Europa/Runtime.hs
+++ Europa/Runtime.hs
@@ -1,89 +1,78 @@
 module Europa.Runtime
-    ( Code(..), Term(..), ap, tap
-    , convertible, normalize
-    , box, sortOf, typeOf
+    ( Code(..), Term(..), ap
+    , convertible
+    , box, typeOf
     , runChecks) where
 
 import Control.Monad hiding (ap)
 import qualified Data.ByteString.Char8 as B
 import Text.Show.Functions
+import Data.Maybe (fromJust)
+import Prelude hiding (pi)
 
 
 data Code = Var !Int
           | Con !B.ByteString
           | Lam !(Code -> Code)
+          | Pi  !Code !(Code -> Code)
           | App !Code !Code
-            deriving Show
+          | Type
+          | Kind
+            deriving (Eq, Show)
+
+instance Eq (Code -> Code)
 
 data Term = TLam !Term !(Term -> Term)
           | TPi  !Term !(Term -> Term)
           | TApp !Term !Term
-          | TCon !B.ByteString
-          | TBox !Term
           | TType
-          | TKind
+          | Box Code Code
             deriving Show
 
 ap :: Code -> Code -> Code
 ap (Lam f) t = f t
 ap t1 t2 = App t1 t2
 
-tap :: Term -> Term -> Term
-tap (TLam _ f) t = f t
-tap t1 t2 = TApp t1 t2
+obj :: Box -> Code
+obj (Box _ obj) = obj
+
+incr x = if x >= 0 then x + 1 else x - 1
 
 convertible :: Int -> Code -> Code -> Bool
 convertible n (Var x) (Var x') = x == x'
 convertible n (Con c) (Con c') = c == c'
 convertible n (Lam t) (Lam t') =
-    convertible (n + 1) (t (Var n)) (t' (Var n))
+    convertible (incr n) (t (Var n)) (t' (Var n))
+convertible n (Pi ty1 ty2) (Pi ty3 ty4) =
+    convertible n ty1 ty3 && convertible (incr n) (ty2 (Var n)) (ty4 (Var n))
 convertible n (App t1 t2) (App t3 t4) =
     convertible n t1 t3 && convertible n t2 t4
+convertible n Type Type = True
+convertible n Kind Kind = True
 convertible n _ _ = False
 
-normalize :: Int -> Code -> Code
-normalize n (Lam t) = normalize (n+1) (t (Var n))
-normalize n (App t1 t2) = App t1 (normalize n t2)
+box :: Term -> Code -> Code -> Term
+box ty ty_code obj_code | Just Type <- typeOf (-1) ty = Box ty_code obj_code
+                        | Just Kind <- typeOf (-1) ty = Box ty_code obj_code
+                        | otherwise = error "box exception."
 
-sortOf :: Term -> Maybe Term
-sortOf ty = do
-  sort <- typeOf ty
-  case sort of
-    TType -> return TType
-    TKind -> return TKind
-    _ -> mzero
-
-box :: Term -> Maybe Term
-box ty = do sortOf ty; return $ TBox ty
-
--- We only rewrite inside boxes, and constants only appear as a consequence of
--- an application of a rewrite rule, so we don't need to type constants.
-typeOf :: Term -> Maybe Term
--- rule: Sort
-typeOf TType = return TKind
--- rule: Annotated term
--- Invariant: the type of a box has always been checked prior to its usage.
-typeOf (TBox ty) = return ty
--- rules: Abstraction 1 and 2
-typeOf (TLam ty t) = do
-  TType <- typeOf ty
-  return $ TPi ty t
--- rules: Product 1 and 2
-typeOf (TPi tyd tyr) = do
-  TType <- typeOf tyd
-  sortOf (tyr (TBox tyd))
--- rule: Application
-typeOf (TApp t1 t2) = do
-  TPi tyd tyr <- typeOf t1
-  t2ty <- typeOf t2
---  guard (convertible tyd t2ty)
-  return (tyr (TBox tyd))
--- otherwise typeChecking fails
-typeOf _ = mzero
+typeOf :: Int -> Term -> Maybe Code
+typeOf n (Box ty _) = return ty
+typeOf n (TLam bx@(Box Type a) f) = do
+  return $ Pi a (\x -> fromJust $ typeOf n (f (Box a x)))
+typeOf n (TPi bx@(Box Type a) f) = do
+  typeOf (incr n) (f (Box a (Var n)))
+typeOf n (TApp t1 bx@(Box ty2 t2)) = do
+  Pi tya f <- typeOf n t1
+  guard (convertible (-1) tya ty2)
+  return $ f t2
+typeOf n TType = return Kind
+typeOf n t = error (show t)
 
 -- | Check that all items in the list are of sort Type or Kind.
-runChecks :: [Maybe Term] -> IO ()
-runChecks = mapM_ check where
-    check (Just TType) = return ()
-    check (Just TKind)  = return ()
-    check _            = fail "Sort check failure."
+runChecks :: [Maybe Code] -> IO ()
+runChecks = undefined
+-- runChecks = mapM_ check where
+--     check (Just Type) = return ()
+--     check (Just Kind)  = return ()
+--     check _            = fail "Sort check failure."
