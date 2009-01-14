009bfb04f1a07c95496a351fa2085d948c345308
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-01-14 14:13:26 +0100

    New Interactive driver.
    Mainly useful for debugging at the moment. This driver is not still
    skeletal.

---
+++ Europa/Driver/Interactive.hs
@@ -0,0 +1,16 @@
+module Europa.Driver.Interactive (eval) where
+
+import Europa.Module
+import Europa.Parser
+import qualified Europa.CodeGen.Exts as CG
+import qualified Europa.Rule as Rule
+import Data.ByteString (ByteString)
+
+
+-- | Emit Haskell code for one module.
+eval :: String -> ByteString
+eval input =
+    let (decls, rules) = parse "<interactive>" input
+        code = map CG.emit (Rule.ruleSets decls rules) :: [CG.Code]
+        mod = Module (hierarchy ["interactive"])
+    in CG.serialize (undefined :: CG.Code) mod $ CG.coalesce code
