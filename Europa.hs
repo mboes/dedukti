447140dbdd78ca66c6d7d1a97b69ea7ac07d69ee
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-05-06 16:44:42 +0200

    Rewrite stub main into a proper one.
    Parses command line using GetOpt and dispatches to the correct driver
    depending on options.

--- Europa.hs
+++ Europa.hs
@@ -3,30 +3,43 @@ module Main where
 import System.Environment
 import Europa.Config as Config
 import Europa.EuM
-import Text.PrettyPrint.Leijen
-import System.Exit
-import System.IO
 import Europa.Pretty
 import Europa.Parser
+import Europa.Module
+import Europa.Driver.Batch
+import Europa.Driver.Compile
+import Text.PrettyPrint.Leijen
+import Control.Monad (when)
+import System.Console.GetOpt
+import System.Exit
+import System.IO
 
 
-printUsage = do
-  self <- runEuM $ select Config.imageName
-  putDoc $ text self <+> text "[OPTION]..." <+> text "FILE"
+data Flag = Make
+            deriving (Eq, Ord, Show)
+
+options = [ Option [] ["make"] (NoArg Make)
+                       "Build FILE and all its dependencies in one go." ]
+
+data Usage = Help | WrongUsage
+
+printUsage format = do
+  self <- parameter Config.imageName
+  let header = show $ text "Usage:"
+               <+> text self <+> text "[OPTION]..." <+> text "FILE"
+  case format of
+    Help -> io $ putStrLn (usageInfo header options) >> exitSuccess
+    WrongUsage -> io $ hPutStrLn stderr header >> exitFailure
 
 main = do
   args <- getArgs
-  (filename, handle) <-
-      case args of
-        [path] -> do h <- openFile path ReadMode
-                     return (path, h)
-        _ -> do printUsage
-                exitFailure
-  input <- hGetContents handle
-  case parse filename input of
-    Left e -> error (show e)
-    Right (decls, rules) -> do
-         putStrLn "Rules:\n"
-         putStrLn . show . pretty $ rules
-         putStrLn "\nDeclarations:\n"
-         putStrLn . show . pretty $ decls
+  let (opts, args, errs) = getOpt Permute options args
+  when (length args /= 1 || not (null errs)) $ do
+         hPutDoc stderr (vsep (map text errs))
+         exitFailure
+  let mod = moduleFromPath (head args)
+  runEuM Config.defaultConfig $
+         case opts of
+           _ | Make `elem` opts -> make [mod]
+             | otherwise -> compile mod
+
