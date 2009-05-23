945afc1bfced7386b1793346eb4021b8283d5245
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-05-23 16:12:21 +0200

    xencode Qid's.

--- Europa/CodeGen/Exts.hs
+++ Europa/CodeGen/Exts.hs
@@ -33,37 +33,61 @@ instance CodeGen Record where
         where main = Hs.FunBind [Hs.Match (!) (Hs.Ident "main") []
                                        Nothing (Hs.UnGuardedRhs checks) (Hs.BDecls [])]
               checks = primitiveVar "runChecks"
-                       [Hs.List $ map (var . (.$ "_box") . rec_name) records]
+                       [Hs.List $ map (var . (.$ "box") . rec_name) records]
 
     serialize _ (Module mod) decls =
         B.pack $ prettyPrint $
         Hs.Module (!) modname [] Nothing Nothing imports decls
         where imports = [ Hs.ImportDecl (!) (Hs.ModuleName "Europa.Runtime")
                                         False False Nothing Nothing ]
-              modname = Hs.ModuleName $ intercalate "." $ map (upcase . B.unpack) (toList mod)
+              modname = Hs.ModuleName $ B.unpack $ B.intercalate "." $
+                        map upcase $ toList mod
 
     emit rs@(RS x ty rules) = Rec x ty [function rs, def_ty, def_box]
-        where def_ty  = value (x .$ "_ty") (code ty)
-              def_box = value (x .$ "_box")
-                                     (primbbox (term ty) (var (x .$ "_ty")) (var x))
+        where def_ty  = value (x .$ "ty") (code ty)
+              def_box = value (x .$ "box")
+                                     (primbbox (term ty) (var (x .$ "ty")) (var x))
+
+-- | A similar encoding of names as the z-encoding of GHC. Non-letter
+-- | characters are escaped with an x.
+xencode :: Qid -> String
+xencode qid =
+    B.unpack $
+     joinQ (qid_qualifier qid) `B.append`
+     -- Prepend all idents with an x to avoid clash with runtime functions.
+     B.cons 'x' (enc (qid_stem qid)) `B.append`
+     joinS (qid_suffix qid)
+        where joinQ Root = ""
+              joinQ (h :. x) = joinQ h `B.append` upcase x `B.append` "."
+              joinS Root = ""
+              joinS (h :. x) = joinS h `B.append` "_" `B.append` x
+              enc = B.concatMap f where
+                  f 'x'  = "xx"
+                  f '\'' = "xq"
+                  f '_'  = "xu"
+                  f x | x >= '0', x <= '9' = 'x' `B.cons` B.singleton x
+                      | otherwise = B.singleton x
 
 function :: Em RuleSet -> Hs.Decl
-function (RS x _ []) = Hs.FunBind [defaultClause x]
+function (RS x _ []) =
+    Hs.FunBind [Hs.Match (!) (varName x) [] Nothing (Hs.UnGuardedRhs (primCon x)) (Hs.BDecls [])]
 function (RS x _ rs) =
     Hs.FunBind [Hs.Match (!) (varName x) [] Nothing (Hs.UnGuardedRhs rhs) (Hs.BDecls [f])]
     where n = Rule.arity (head rs)
-          rhs = foldr primLam (application (var (qid "__") : variables n)) (pvariables n)
-          f | n > 0     = Hs.FunBind (map (clause (qid "__")) rs ++ [defaultClause x])
-            | otherwise = Hs.FunBind (map (clause (qid "__")) rs)
+          rhs = foldr primLam
+                (application (Hs.Var (Hs.UnQual (Hs.Ident "__")) : variables n))
+                (pvariables n)
+          f | n > 0     = Hs.FunBind (map clause rs ++ [defaultClause x])
+            | otherwise = Hs.FunBind (map clause rs)
 
-clause :: Id Record -> Em TyRule -> Hs.Match
-clause x rule@(env :@ (lhs :--> rhs)) =
-    Hs.Match (!) (varName x) (map (pattern env) (Rule.patterns rule))
+clause :: Em TyRule -> Hs.Match
+clause rule@(env :@ (lhs :--> rhs)) =
+    Hs.Match (!) (Hs.Ident "__") (map (pattern env) (Rule.patterns rule))
           Nothing (Hs.UnGuardedRhs (code rhs)) (Hs.BDecls [])
 
 defaultClause :: Id Record -> Hs.Match
 defaultClause x =
-    Hs.Match (!) (varName x) [] Nothing (Hs.UnGuardedRhs (primCon x)) (Hs.BDecls [])
+    Hs.Match (!) (Hs.Ident "__") [] Nothing (Hs.UnGuardedRhs (primCon x)) (Hs.BDecls [])
 
 value :: Id Record -> Hs.Exp -> Hs.Decl
 value x rhs =
@@ -87,7 +111,7 @@ code Type                 = primType
 
 -- | Turn a term into its Haskell representation, including all types.
 term :: Em Expr -> Hs.Exp
-term (Var x _)     = var (x .$ "_box")
+term (Var x _)     = var (x .$ "box")
 term (Lam b t _)   = primTLam b (term t)
 term (Pi b t _)    = primTPi  b (term t)
 term (App t1 t2 _) = primTApp (term t1) (primUBox (term t2) (code t2))
@@ -98,10 +122,10 @@ term Kind          = primTKind
 (!) = Hs.SrcLoc "" 0 0
 
 varName :: Id Record -> Hs.Name
-varName x = Hs.Ident $ 'x' : symbolize x
+varName x = Hs.Ident $ xencode x
 
 conName :: Id Record -> Hs.Name
-conName x = Hs.Ident $ 'X': symbolize x
+conName x = Hs.Ident $ xencode x
 
 -- | Smart variable constructor.
 var :: Id Record -> Hs.Exp
@@ -136,7 +160,7 @@ primitiveCon s xs = Hs.Paren $ application $ (Hs.Con $ Hs.UnQual $ Hs.Ident s) :
 
 primap  t1 t2 = primitiveVar "ap"  [t1, t2]
 primApp t1 t2 = primitiveCon "App" [t1, t2]
-primCon c     = primitiveCon "Con" [Hs.Lit (Hs.String ('X': symbolize c))]
+primCon c     = primitiveCon "Con" [Hs.Lit (Hs.String (xencode c))]
 primType      = primitiveCon "Type" []
 primKind      = primitiveCon "Kind" []
 
@@ -148,9 +172,9 @@ primApps c = foldl primApp (primCon c)
 typedAbstraction c b t =
     let (pat, ty, ran) =
             case b of
-              x ::: ty -> ( pvar (x .$ "_box")
+              x ::: ty -> ( pvar (x .$ "box")
                           , ty
-                          , Hs.Let (Hs.BDecls [value x (primobj (var (x .$ "_box")))]) t )
+                          , Hs.Let (Hs.BDecls [value x (primobj (var (x .$ "box")))]) t )
               Hole ty  -> (Hs.PWildCard, ty, t)
         dom = if isVariable ty
               then term ty else primsbox (term ty) primType (code ty)
@@ -172,11 +196,12 @@ primobj t = primitiveVar "obj" [t]
 primtypeOf t = primitiveVar "typeOf" [t]
 
 -- | Build a pattern matching a constant.
-primConP c = Hs.PParen $ Hs.PApp (Hs.UnQual $ Hs.Ident "Con") [Hs.PLit (Hs.String ('X': symbolize c))]
+primConP c = Hs.PParen $ Hs.PApp (Hs.UnQual $ Hs.Ident "Con") [Hs.PLit (Hs.String (xencode c))]
 primAppP t1 t2 = Hs.PParen $ Hs.PApp (Hs.UnQual $ Hs.Ident "App") [t1, t2]
 primAppsP c = foldl primAppP (primConP c)
 
 -- | Upcase a word.
-upcase :: String -> String
-upcase [] = ""
-upcase (x:xs) = toUpper x : xs
+upcase :: B.ByteString -> B.ByteString
+upcase s = case B.uncons s of
+             Nothing -> ""
+             Just (x, xs) -> toUpper x `B.cons` xs
