fbd3b91569d5e4b2f55cd32017f659b255ba5e5f
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-06-10 18:05:47 +0200

    Split backend part of 'compile' into 'compileAST'.
    This is to avoid having to reparse stuff when we've already parsed it.

--- Europa/Driver/Compile.hs
+++ Europa/Driver/Compile.hs
@@ -4,12 +4,13 @@
 --
 -- Compile one file to Haskell source code.
 
-module Europa.Driver.Compile (compile) where
+module Europa.Driver.Compile (compile, compileAST) where
 
 import Europa.Module
 import Europa.Parser
 import Europa.EuM
 import Europa.Core
+import Europa.Analysis.Dependency
 import qualified Europa.CodeGen.Exts as CG
 import qualified Europa.Rule as Rule
 import qualified Data.ByteString.Lazy.Char8 as B
@@ -23,8 +24,13 @@ dump mod = io . B.writeFile (objPathFromModule mod)
 -- | Emit Haskell code for one module.
 compile :: Module -> EuM ()
 compile mod = do
-  say Verbose $ text "Compiling" <+> text (show mod) <+> text "..."
+  say Verbose $ text "Parsing" <+> text (show mod) <+> text "..."
   let path = srcPathFromModule mod
-  (decls, rules) <- return (parse path) `ap` io (B.readFile path)
+  compileAST mod =<< return (parse path) `ap` io (B.readFile path)
+
+compileAST :: Module -> ([Pa Binding], [Pa TyRule]) -> EuM ()
+compileAST mod src@(decls, rules) = do
+  say Verbose $ text "Compiling" <+> text (show mod) <+> text "..."
   let code = map CG.emit (Rule.ruleSets decls rules) :: [CG.Code]
-  dump mod $ CG.serialize mod $ CG.coalesce code
+      deps = collectDependencies src
+  dump mod $ CG.serialize mod deps $ CG.coalesce code
