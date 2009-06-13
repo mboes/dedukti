c1facb06f0b5444e7d98d2c10eb58aa4e4bee9e3
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-06-13 13:12:52 +0200

    Self-qualify all occurrences of toplevel module identifiers.

--- Europa/Driver/Compile.hs
+++ Europa/Driver/Compile.hs
@@ -14,11 +14,23 @@ import Europa.Analysis.Dependency
 import qualified Europa.CodeGen.Exts as CG
 import qualified Europa.Rule as Rule
 import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Set as Set
 import Control.Monad (ap)
 
 
 dump :: Module -> B.ByteString -> EuM ()
 dump mod = io . B.writeFile (objPathFromModule mod)
+
+-- | Qualify all occurrences of identifiers defined in current module.
+selfQualify :: Module -> [RuleSet Qid a] -> [RuleSet Qid a]
+selfQualify (Module mod) rsets = let defs = Set.fromList (map rs_name rsets)
+                                 in map (descend (f defs))
+                                        (map (\RS{..} -> RS{rs_name = rs_name{qid_qualifier = mod}, ..}) rsets)
+    where f defs (Var x a) | Nothing <- provenance x
+                           , x `Set.member` defs = Var x{qid_qualifier = mod} a
+          f defs (Lam (x ::: ty) t a) = Lam (x ::: f defs ty) (f (Set.delete x defs) t) a
+          f defs (Pi (x ::: ty) t a)  = Pi (x ::: f defs ty) (f (Set.delete x defs) t) a
+          f defs t = descend (f defs) (t :: Expr Qid a)
 
 -- | Emit Haskell code for one module.
 compile :: Module -> EuM ()
@@ -30,6 +42,6 @@ compile mod = do
 compileAST :: Module -> ([Pa Binding], [Pa TyRule]) -> EuM ()
 compileAST mod src@(decls, rules) = do
   say Verbose $ text "Compiling" <+> text (show mod) <+> text "..."
-  let code = map CG.emit (Rule.ruleSets decls rules) :: [CG.Code]
+  let code = map CG.emit (selfQualify mod (Rule.ruleSets decls rules)) :: [CG.Code]
       deps = collectDependencies src
   dump mod $ CG.serialize mod deps $ CG.coalesce code
