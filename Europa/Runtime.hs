5d6d1bf5e0bfddd9c6f51675ed8cbf474deee1fc
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-06-15 20:50:08 +0200

    Relicense Europa/Runtime.hs file under and ISC-style license.

--- Europa/Runtime.hs
+++ Europa/Runtime.hs
@@ -1,10 +1,20 @@
--- |
--- Copyright : (c) 2009 INRIA
--- License   : GPL
+-- Copyright (c) 2009 INRIA
 --
--- All generated Haskell files import this module. The data type declarations
--- are given here, along with the conversion relation and type inference
--- function.
+-- Permission to use, copy, modify, and distribute this file for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+-- | All generated Haskell files import this module. The data type
+-- declarations are given here, along with the conversion relation and type
+-- inference function.
 
 module Europa.Runtime
     ( Code(..), Term(..), ap
