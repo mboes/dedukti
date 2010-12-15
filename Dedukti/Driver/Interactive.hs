44c49c634628e660f068d349dd20d6403e0dc382
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2010-12-15 21:12:05 +0100

    Remove old interactive code.

--- Dedukti/Driver/Interactive.hs
+++ Dedukti/Driver/Interactive.hs
@@ -6,18 +6,7 @@ module Dedukti.Driver.Interactive (eval) where
 
 import Dedukti.Module
 import Dedukti.Parser
-import qualified Dedukti.CodeGen.Exts as CG
 import qualified Dedukti.Rule as Rule
 import Data.ByteString.Lazy (ByteString)
 
 
-dk = QuasiQuote { quoteExp = parse "<interactive>"
-                , quoteDec = undefined }
-
--- | Emit Haskell code for one module.
-eval :: String -> ByteString
-eval input =
-    let (decls, rules) = parse "<interactive>" input
-        code = map CG.emit (Rule.ruleSets decls rules) :: [CG.Code]
-        mod = Module (hierarchy ["interactive"])
-    in CG.serialize (undefined :: CG.Code) mod $ CG.coalesce code
